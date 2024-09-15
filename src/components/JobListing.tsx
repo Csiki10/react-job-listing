1f710d6335aac56ab270085f03e7ce106cd5b485
benedek.csikos <csikosbenedek@gmail.com>
2024-09-15 20:49:20 +0200

    adjust

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -39,7 +39,7 @@ const JobListing = ({ job }: JobListingProps) => {
             {job.location}
           </div>
           <Link
-            to={`/jobs/${job.id}`}
+            to={`/jobs/?id=${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
